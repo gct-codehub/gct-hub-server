a0fc443f5a07989e151b147a46bfdb1c0daab492
Adithya <64605932+elizzer@users.noreply.github.com>
2023-07-18 20:52:00 +0530

    test changes

--- middleware/isLoggedIn.js
+++ middleware/isLoggedIn.js
@@ -7,7 +7,7 @@ export const isLoggedIn = async (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
     if (token) {
-      const decoded = jwt.verify(token, process.env.JWT_KEY);
+      const decoded =  jwt.verify(token, process.env.ACCESS_TOKEN);
       req.params.id = decoded?._id;
     }
     next();
